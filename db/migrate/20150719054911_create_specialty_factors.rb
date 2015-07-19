7aa34f5e1fb99e8b0e3853641f8156d5dcaf5e17
Matthew Vasseur <mattvasseur@gmail.com>
2015-07-19 02:24:46 -0400

    pre-migrate

--- db/migrate/20150719054911_create_specialty_factors.rb
+++ db/migrate/20150719054911_create_specialty_factors.rb
@@ -1,4 +1,4 @@
-class CreateSpecialties < ActiveRecord::Migration
+class CreateSpecialtyFactors < ActiveRecord::Migration
   def change
     create_table :specialty_factors do |t|
       t.string :spec_name
