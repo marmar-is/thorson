04f52fba4d3ed58ad294abd3c1a81ca73093f1bf
Matthew Vasseur <mattvasseur@gmail.com>
2015-07-23 19:27:30 -0400

    rate+factor tables look good

--- app/controllers/factors_controller.rb
+++ app/controllers/factors_controller.rb
@@ -10,6 +10,10 @@ class FactorsController < ApplicationController
     @territory_factors   = TerritoryFactor.all
     @spec_factors        = SpecialtyFactor.all
     @risk_factors        = RiskFactor.all
+
+    @nas_rates           = NasRate.all
+    @base_rates          = BaseRate.all
+    @allied_rates        = AlliedRate.all
   end
 
   protected
