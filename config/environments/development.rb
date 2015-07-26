2c083d7192351bd4d934f8a803fde7658299f180
Matthew Vasseur <mattvasseur@gmail.com>
2015-07-26 11:12:07 -0400

    sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # User letter_opener for development emails
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   # Default mailer url
   config.action_mailer.default_url_options = { host: 'localhost', port: 8080 }
