45df97b1de682b109ec50d48c803c83e90b3561a
Matt Vasseur <matt@marmar.is>
2015-08-15 16:01:29 -0400

    production.rb unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -14,19 +14,19 @@ preload_app true
 listen "#{shared_path}/sockets/unicorn.Thorson.sock", backlog: 64
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+  #Signal.trap 'TERM' do
+  #  puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+  #  Process.kill 'QUIT', Process.pid
+  #end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
+  #Signal.trap 'TERM' do
+  #  puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  #end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
